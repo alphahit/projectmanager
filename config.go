53c99f56cd72a7182b32dcc57d5156783e64783b
alphahit <prateekpriyadarshi1328@gmail.com>
2024-07-05 23:02:23 +0530

    db password removed

--- config.go
+++ config.go
@@ -18,12 +18,12 @@ var Envs = initConfig()
 
 func initConfig() Config {
 	return Config{
-		Port:       getEnv("PORT", "8080"),
-		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "password"),
-		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "33006")),
-		DBName:     getEnv("DB_NAME", "projectmanager"),
-		JWTSecret:  getEnv("JWTSECRET", "randomjwtsecurekey"),
+		Port:   getEnv("PORT", "8080"),
+		DBUser: getEnv("DB_USER", "root"),
+		// DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBAddress: fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBName:    getEnv("DB_NAME", "projectmanager-mysql"),
+		JWTSecret: getEnv("JWTSECRET", "randomjwtsecurekey"),
 	}
 }
 
